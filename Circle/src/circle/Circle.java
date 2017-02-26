be9b4d5c7372f402587630c78bce389c5b2d632e
ROMIK <pelipec1980@gmail.com>
2017-02-26 16:55:10 +0300

    circle

--- Circle/src/circle/Circle.java
+++ Circle/src/circle/Circle.java
@@ -1,19 +1,21 @@
 package circle;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class Circle {
 	private static double d;
 	private static double s;
-	private static double p = 3.14;
-	public static void main(String[] args)throws  IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("INPUT NUMBER");
-			double r = Double.parseDouble(br.readLine());
-			d = 2 * r;
-			System.out.println(d);
-			s = 2 *( p *( r*2));
-			System.out.println(s);
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("INPUT NUMBER");
+		double r = Double.parseDouble(br.readLine());
+		d = 2 * r;
+		System.out.println(d);
+		s = 2 * (Math.PI * d);
+		System.out.println(s);
 
 	}
 
